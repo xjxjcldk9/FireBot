00c87180d6702183bd3c0fa4e60f1952921ce48f
Shen <xjxjcldk9@gmail.com>
2024-10-14 23:47:49 +0800

    add: discord bot

--- users/tester.py
+++ users/tester.py
@@ -7,9 +7,7 @@ from firebot.users import USERS
 
 load_dotenv()
 
-
-user = USERS('tester',
-             os.getenv('TESTER'),
+user = USERS('tester', os.getenv('TESTER'), os.getenv('TEST_WEB_HOOK'),
              lambda x: True)
 
 fire_bot_worker(user)
